dfdad3526cc59486915074b3d3d963ce528e22da
notTanveer <tanveer.x.ansari@gmail.com>
2025-12-10 21:16:08 +0530

    ADD: android rust bridge

---
+++ android/app/src/main/java/org/bitshala/shroud/RustJsiBridgeModule.java
@@ -0,0 +1,33 @@
+package io.bluewallet.bluewallet;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = RustJsiBridgeModule.NAME)
+public class RustJsiBridgeModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "RustJsiBridge";
+
+    public RustJsiBridgeModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public boolean install() {
+        try {
+            System.loadLibrary("rust-jsi-bridge");
+            nativeInstall(getReactApplicationContext().getJavaScriptContextHolder().get());
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    private native void nativeInstall(long jsiPtr);
+}
