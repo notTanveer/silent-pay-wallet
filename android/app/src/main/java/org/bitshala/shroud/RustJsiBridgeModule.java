32905c2520f30c249e6e32ad3c60421bbd76fcee
notTanveer <tanveer.x.ansari@gmail.com>
2025-12-11 17:16:10 +0530

    REF: use org.bitshala.shroud

--- android/app/src/main/java/org/bitshala/shroud/RustJsiBridgeModule.java
+++ android/app/src/main/java/org/bitshala/shroud/RustJsiBridgeModule.java
@@ -1,4 +1,4 @@
-package io.bluewallet.bluewallet;
+package org.bitshala.shroud;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
