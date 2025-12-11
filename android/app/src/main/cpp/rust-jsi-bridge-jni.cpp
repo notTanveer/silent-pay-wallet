3e6c08e79f6ba6f17d63b8dffaaaebdce04f7197
notTanveer <tanveer.x.ansari@gmail.com>
2025-12-11 17:16:10 +0530

    REF: use org.bitshala.shroud

--- android/app/src/main/cpp/rust-jsi-bridge-jni.cpp
+++ android/app/src/main/cpp/rust-jsi-bridge-jni.cpp
@@ -5,7 +5,7 @@
 using namespace facebook::jsi;
 
 extern "C" JNIEXPORT void JNICALL
-Java_io_bluewallet_bluewallet_RustJsiBridgeModule_nativeInstall(
+Java_org_bitshala_shroud_RustJsiBridgeModule_nativeInstall(
     JNIEnv *env,
     jobject thiz,
     jlong jsiRuntimePtr
