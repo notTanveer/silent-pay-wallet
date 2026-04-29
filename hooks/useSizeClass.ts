5e0b0f400fd8be66bf454d9fb90cde2c66daa065
notTanveer <tanveer.x.ansari@gmail.com>
2026-04-30 00:55:17 +0530

    DEL: unused files

--- hooks/useSizeClass.ts
+++
@@ -1,9 +0,0 @@
-import { useSizeClass as useSizeClassOriginal, SizeClass } from '../modules/sizeClass';
-import type { SizeClassInfo } from '../modules/sizeClass';
-
-export { SizeClass };
-export type { SizeClassInfo };
-
-export const useSizeClass = useSizeClassOriginal;
-
-export const useIsLargeScreen = useSizeClassOriginal;
