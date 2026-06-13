de1d7d5f7f9680514cf595f45dc1219aeee850c6
chaitika <chaitikab@gmail.com>
2026-06-13 14:00:51 +0530

    TST: remove dead commented block in ecc test

--- tests/unit/ecc.test.ts
+++ tests/unit/ecc.test.ts
@@ -23,31 +23,6 @@ describe('ecc', () => {
     );
     assert.ok(!ecc.isPoint(Buffer.from('00', 'hex')));
 
-    /*
-
-    muted because of that:
-
-    ```
-        if (!isWithinCurveOrder(num))
-        throw new Error('Expected private key: 0 < key < n');
-    ````
-
-    in `node_modules/@noble/secp256k1/lib/index.js`
-    (this test runs in runtime in some versions if `ECPairFactory`)
-
-
-    const rez = ecc.privateAdd(
-      h('0000000000000000000000000000000000000000000000000000000000000001'),
-      h('0000000000000000000000000000000000000000000000000000000000000000'),
-    );
-
-
-    assert.strictEqual(
-      Buffer.from(rez).toString('hex'),
-      h('0000000000000000000000000000000000000000000000000000000000000001').toString('hex'),
-    );
-*/
-
     const rez2 = ecc.privateAdd(
       h('fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd036413e'),
       h('0000000000000000000000000000000000000000000000000000000000000003'),
